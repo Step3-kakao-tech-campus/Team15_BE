3e085f74b2c4767cff8661249b21cc62ad277f26
Park Chan <zxc88kr@naver.com>
2023-10-02 06:19:32 +0900

    build: 개발환경 기초 세팅

---
+++ src/main/java/com/kakao/borrowme/BorrowmeApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.borrowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorrowmeApplication.class, args);
+    }
+
+}
