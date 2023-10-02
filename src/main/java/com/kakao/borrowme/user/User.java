7fbb95d8e6b8062be3069ab63211918b596b1282
Park Chan <zxc88kr@naver.com>
2023-10-03 06:49:24 +0900

    feat: JPA Auditing 대상 클래스 지정

--- src/main/java/com/kakao/borrowme/user/User.java
+++ src/main/java/com/kakao/borrowme/user/User.java
@@ -1,4 +1,9 @@
 package com.kakao.borrowme.user;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 }
