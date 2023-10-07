f9999c370a4f9158912ecc84fa311485e8fe79eb
Park Chan <zxc88kr@naver.com>
2023-10-08 06:14:12 +0900

    feat: JoinDTO 생성

---
+++ src/main/java/com/kakao/borrowme/user/UserRequest.java
@@ -0,0 +1,28 @@
+package com.kakao.borrowme.user;
+
+import com.kakao.borrowme.university.University;
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserRequest {
+    @Getter @Setter
+    public static class JoinDTO {
+        private String universityName;
+        private String email;
+        private String password;
+        private String nickname;
+
+        public User toEntity() {
+            return User.builder()
+                    .university(
+                            University.builder()
+                                    .name(universityName)
+                                    .build())
+                    .email(email)
+                    .password(password)
+                    .nickname(nickname)
+                    .role("student")
+                    .build();
+        }
+    }
+}
