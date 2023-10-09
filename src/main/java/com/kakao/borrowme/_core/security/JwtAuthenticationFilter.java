83a51b4fe15cc130a57957918815fb4ecb83a41d
Park Chan <zxc88kr@naver.com>
2023-10-10 04:20:16 +0900

    feat: authentication 객체 생성 구현

--- src/main/java/com/kakao/borrowme/_core/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kakao/borrowme/_core/security/JwtAuthenticationFilter.java
@@ -1,6 +1,14 @@
 package com.kakao.borrowme._core.security;
 
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.kakao.borrowme.user.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
@@ -19,6 +28,26 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         String jwt = request.getHeader(JWTProvider.HEADER);
 
         if (jwt == null) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            DecodedJWT decodedJWT = JWTProvider.verify(jwt);
+            Long id = decodedJWT.getClaim("id").asLong();
+            String nickname = decodedJWT.getClaim("role").asString();
+            User user = User.builder().id(id).nickname(nickname).build();
+
+            CustomUserDetails myUserDetails = new CustomUserDetails(user);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    myUserDetails, myUserDetails.getPassword(), myUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("디버그: 인증 객체 만들어짐");
+        } catch (SignatureVerificationException sve) {
+            log.error("토큰 검증 실패");
+        } catch (TokenExpiredException tee) {
+            log.error("토큰 만료됨");
+        } finally {
             chain.doFilter(request, response);
         }
     }
