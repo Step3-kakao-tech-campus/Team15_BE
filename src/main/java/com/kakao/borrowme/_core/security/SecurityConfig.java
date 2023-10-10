d9d67c9dfa09f1c727e7ea8c9e25cd113e757640
Park Chan <zxc88kr@naver.com>
2023-10-10 10:41:50 +0900

    feat: CORS 재설정

--- src/main/java/com/kakao/borrowme/_core/security/SecurityConfig.java
+++ src/main/java/com/kakao/borrowme/_core/security/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -37,6 +40,9 @@ public class SecurityConfig {
         // 6. CustomSecurityFilter 적용
         http.apply(new CustomSecurityFilterManager());
 
+        // 7. CORS 재설정
+        http.cors().configurationSource(configurationSource());
+
         return http.build();
     }
 
@@ -47,5 +53,17 @@ public class SecurityConfig {
             http.addFilter(new JwtAuthenticationFilter(authenticationManager));
             super.configure(http);
         }
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 -> 프론트 IP 접근 한정 필요
+        configuration.setAllowCredentials(true); // 클라이언트 쿠키 요청 허용
+        configuration.addExposedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
