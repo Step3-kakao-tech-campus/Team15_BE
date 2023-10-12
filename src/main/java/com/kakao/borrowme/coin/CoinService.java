e140e24b382e82ca86c9a223b4229f430a9651c3
jihyelee <110345050+C0Zl@users.noreply.github.com>
2023-10-12 15:08:19 +0900

    refactor : request 변경 반영 및 에러 수정

--- src/main/java/com/kakao/borrowme/coin/CoinService.java
+++ src/main/java/com/kakao/borrowme/coin/CoinService.java
@@ -1,82 +1,92 @@
 package com.kakao.borrowme.coin;
 
-import com.kakao.borrowme._core.errors.Exception400;
+import com.kakao.borrowme.product.Product;
+import com.kakao.borrowme.product.ProductJPARepository;
 import com.kakao.borrowme.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
+@RequiredArgsConstructor
+@Slf4j
 @Service
 public class CoinService {
 
     private final CoinJPARepository coinJPARepository;
-
-    @Autowired
-    public CoinService(CoinJPARepository coinJPARepository) {
-        this.coinJPARepository = coinJPARepository;
-    }
+    private final ProductJPARepository productJPARepository;
 
     public CoinResponse.FindByIdDTO getUserCoin(User user) {
 
-        Optional<Coin> coinOptional = coinJPARepository.findByUserId(user.getId());
-        Coin coin;
+        Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
 
-        if (coinOptional.isPresent()) {
-            coin = coinOptional.get();
-        } else {
-            // 코인 정보가 없을 경우 기본값으로 0으로 설정
-            coin = Coin.builder()
-                    .user(user)
-                    .piece(0L)
-                    .build();
+        if (!coinOP.isPresent()) {
+            log.error("코인 정보가 없습니다.");
+            return null; // exception 수정 예정
         }
 
-        return new CoinResponse.FindByIdDTO(coin.getPiece());
+        Coin coin = coinOP.get();
+        return new CoinResponse.FindByIdDTO(coin);
+
     }
 
     public CoinResponse.FindByIdDTO chargeCoin(User user, CoinRequest.ChargeCoinDTO chargeCoinDTO) {
 
-        Optional<Coin> coinOptional = coinJPARepository.findByUserId(user.getId());
+        Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
         Coin coin;
         Long piece = chargeCoinDTO.getPiece();
 
-        if (coinOptional.isPresent()) {
-            coin = coinOptional.get();
-            coin.setPiece(coin.getPiece() + piece);
-        } else {
-            coin = Coin.builder()
-                    .user(user)
-                    .piece(piece)
-                    .build();
+        if (!coinOP.isPresent()) {
+            log.error("코인 정보가 없습니다.");
+            return null; // exception 수정 예정
         }
+
+        coin = coinOP.get();
+        coin.setPiece(coin.getPiece() + piece);
 
         coinJPARepository.save(coin);
-
-        return new CoinResponse.FindByIdDTO(coin.getPiece());
+        return new CoinResponse.FindByIdDTO(coin);
 
     }
 
-    public void useCoin(User user, CoinRequest.UseCoinDTO useCoinDTO) {
+    public void useCoin(User user, String productId, LocalDateTime startAt, LocalDateTime endAt) {
 
-        Long totalPrice = useCoinDTO.getTotalPrice();
+        Optional<Product> productOP = ProductJPARepository.findById(productId);
 
-        // * 예외처리 수정 필요 *
-        Optional<Coin> coinOptional = coinJPARepository.findByUserId(user.getId());
+        if (!productOP.isPresent()) {
 
-        if (coinOptional.isPresent()) {
-            Coin coin = coinOptional.get();
-            if (coin.getPiece() >= totalPrice) {
+            // 상품이 존재하지 않는 경우에 대한 예외 처리 필요
+            log.error("상품이 존재하지 않습니다.");
+
+        } else {
+
+            Long rentalPrice = productOP.get().getRentalPrice();
+
+            Long durationInHours = Duration.between(startAt, endAt).toHours(); // 대여 기간 (시간)
+            Long durationInDays = durationInHours / 24; // 대여 기간 (일)
+
+            Long totalPrice = rentalPrice * durationInDays;
+
+            Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
+
+            Coin coin = coinOP.get();
+
+            if (coin.getPiece() < totalPrice) {
+
+                // 코인 잔액이 부족한 경우에 대한 예외 처리
+                log.error("코인이 부족합니다.");
+
+            } else {
+
                 coin.setPiece(coin.getPiece() - totalPrice);
                 coinJPARepository.save(coin);
-                // 결제 로직 추가
-            } else {
-                // 코인 잔액이 부족한 경우 예외 처리
-                throw new Exception400("코인이 부족합니다.");
+
             }
-        } else {
-            // 코인 엔티티가 없는 경우 예외 처리
-            throw new Exception400("코인 정보가 없습니다.");
+
         }
+
     }
+
 }
