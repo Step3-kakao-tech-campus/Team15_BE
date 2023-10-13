8fae2c57ce35268474a8276f774af4c314d791c5
jihyelee <110345050+C0Zl@users.noreply.github.com>
2023-10-13 19:29:20 +0900

    feat : findByProductId 추가
    - 상품 이미지 조회를 위한 findByProductId 추가

--- src/main/java/com/kakao/borrowme/product/image/ProductImageJPARepository.java
+++ src/main/java/com/kakao/borrowme/product/image/ProductImageJPARepository.java
@@ -2,8 +2,9 @@ package com.kakao.borrowme.product.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductImageJPARepository extends JpaRepository<ProductImage, Long> {
     @Query("SELECT pi FROM ProductImage pi WHERE pi.product.id = :productId")
-    ProductImage findByProductId(Long productId);
+    ProductImage findByProductId(@Param("productId")Long productId);
 }
