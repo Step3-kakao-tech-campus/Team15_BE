64952235f16648aa99ee0817d52cb2a22085fd42
jihyelee <110345050+C0Zl@users.noreply.github.com>
2023-10-13 20:02:16 +0900

    refactor : TODO 및 early return 적용

--- src/main/java/com/kakao/borrowme/coin/CoinService.java
+++ src/main/java/com/kakao/borrowme/coin/CoinService.java
@@ -25,9 +25,10 @@ public class CoinService {
 
         Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
 
+        // TODO : 코인 정보가 없는 경우 예외처리
         if (!coinOP.isPresent()) {
             log.error("코인 정보가 없습니다.");
-            return null; // exception 수정 예정
+            return null;
         }
 
         Coin coin = coinOP.get();
@@ -42,9 +43,10 @@ public class CoinService {
         Coin coin;
         Long piece = chargeCoinDTO.getPiece();
 
+        // TODO : 코인 정보가 없는 경우 예외처리
         if (!coinOP.isPresent()) {
             log.error("코인 정보가 없습니다.");
-            return null; // exception 수정 예정
+            return null;
         }
 
         coin = coinOP.get();
@@ -62,36 +64,34 @@ public class CoinService {
 
         if (!productOP.isPresent()) {
 
-            // 상품이 존재하지 않는 경우에 대한 예외 처리 필요
+            // TODO : 상품이 존재하지 않는 경우에 대한 예외 처리 필요
             log.error("상품이 존재하지 않습니다.");
-
-        } else {
-
-            Long rentalPrice = productOP.get().getRentalPrice();
-
-            Long durationInHours = Duration.between(startAt, endAt).toHours(); // 대여 기간 (시간)
-            Long durationInDays = durationInHours / 24; // 대여 기간 (일)
-
-            Long totalPrice = rentalPrice * durationInDays;
-
-            Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
-
-            Coin coin = coinOP.get();
-
-            if (coin.getPiece() < totalPrice) {
-
-                // 코인 잔액이 부족한 경우에 대한 에러처리
-                // throw new create_insufficient_coinCo
-                log.error("코인이 부족합니다.");
-
-            } else {
-
-                coin.setPiece(coin.getPiece() - totalPrice);
-                coinJPARepository.save(coin);
-
-            }
+            return;
 
         }
+
+        Long rentalPrice = productOP.get().getRentalPrice();
+
+        Long durationInHours = Duration.between(startAt, endAt).toHours(); // 대여 기간 (시간)
+        Long durationInDays = durationInHours / 24; // 대여 기간 (일)
+
+        Long totalPrice = rentalPrice * durationInDays;
+
+        Optional<Coin> coinOP = coinJPARepository.findByUserId(user.getId());
+
+        Coin coin = coinOP.get();
+
+        if (coin.getPiece() < totalPrice) {
+
+            // TODO : 코인 잔액이 부족한 경우에 대한 에러처리
+            // throw new create_insufficient_coinCo
+            log.error("코인이 부족합니다.");
+            return;
+
+        }
+
+        coin.setPiece(coin.getPiece() - totalPrice);
+        coinJPARepository.save(coin);
 
     }
 
