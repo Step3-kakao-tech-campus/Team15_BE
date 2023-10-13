4a504a235bf83c6ddd9edba32461a4c41e21186d
jihyelee <110345050+C0Zl@users.noreply.github.com>
2023-10-13 19:28:39 +0900

    feat : findByCategoryId 추가
    - 카테고리 별 상품 조회를 위한 findByCategoryId 추가

--- src/main/java/com/kakao/borrowme/product/ProductJPARepository.java
+++ src/main/java/com/kakao/borrowme/product/ProductJPARepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ProductJPARepository extends JpaRepository<Product, Long> {
     // 이전 페이지의 마지막 아이템 ID를 기준으로 다음 페이지의 데이터를 조회
     @Query("SELECT p FROM Product p WHERE p.id > :lastItemId ORDER BY p.id ASC")
     Page<Product> findNextPage(@Param("lastItemId") Long lastItemId, Pageable pageable);
+
+    List<Product> findByCategoryId(Long categoryId);
 }
