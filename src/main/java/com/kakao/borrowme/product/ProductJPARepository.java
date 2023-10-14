246df88cb682c0b3910462131f4419dd1794ef0f
geunho00 <gghh1220@gmail.com>
2023-10-14 23:22:13 +0900

    refactor: Product 리펙토링

--- src/main/java/com/kakao/borrowme/product/ProductJPARepository.java
+++ src/main/java/com/kakao/borrowme/product/ProductJPARepository.java
@@ -1,13 +1,13 @@
 package com.kakao.borrowme.product;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ProductJPARepository extends JpaRepository<Product, Long> {
-    // 이전 페이지의 마지막 아이템 ID를 기준으로 다음 페이지의 데이터를 조회
-    @Query("SELECT p FROM Product p WHERE p.id > :lastItemId ORDER BY p.id ASC")
-    Page<Product> findNextPage(@Param("lastItemId") Long lastItemId, Pageable pageable);
+    // 이전 페이지의 마지막 제품 ID를 기준으로 다음 페이지의 데이터를 조회
+    @Query("SELECT p FROM Product p WHERE p.id > :lastProductId ORDER BY p.id ASC")
+    Slice<Product> findNextPage(@Param("lastProductId") Long lastProductId, Pageable pageable);
 }
