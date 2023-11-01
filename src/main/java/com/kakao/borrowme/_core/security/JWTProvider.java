b22043b1a985acc0851fad1574470287e78430a5
Park Chan <zxc88kr@naver.com>
2023-11-01 13:03:54 +0900

    feat: 액세스 토큰 생성 및 검증 구현

--- src/main/java/com/kakao/borrowme/_core/security/JWTProvider.java
+++ src/main/java/com/kakao/borrowme/_core/security/JWTProvider.java
@@ -8,28 +8,29 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.kakao.borrowme.user.User;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Component
 public class JWTProvider {
-    public static final Long ACCESS_EXP = 1000L * 60 * 60 * 48;
+    public static final Long ACCESS_EXP = 1000L * 60 * 60;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
     public static final String SECRET = "MySecretKey";
 
     public static String create(User user) {
         String jwt = JWT.create()
-                .withSubject("AccessToken")
-                .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_EXP))
-                .withClaim("id", user.getId())
-                .withClaim("nickname", user.getNickname())
+                .withSubject(user.getId().toString())
+                .withExpiresAt(Timestamp.valueOf(LocalDateTime.now().plusSeconds(ACCESS_EXP)))
+                .withClaim("role", user.getRole())
+                .withClaim("type", "access")
                 .sign(Algorithm.HMAC512(SECRET));
         return TOKEN_PREFIX + jwt;
     }
 
     public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
-        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(SECRET))
-                .build().verify(jwt.replace(JWTProvider.TOKEN_PREFIX, ""));
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(SECRET)).build()
+                .verify(jwt.replace(JWTProvider.TOKEN_PREFIX, ""));
         return decodedJWT;
     }
 }
