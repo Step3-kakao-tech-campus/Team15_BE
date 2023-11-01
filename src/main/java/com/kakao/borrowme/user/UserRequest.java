f002d07bee86741d8ea8c929c1f88e11403d2694
Park Chan <zxc88kr@naver.com>
2023-11-01 21:11:10 +0900

    feat: 이메일 정규표현식 설정

--- src/main/java/com/kakao/borrowme/user/UserRequest.java
+++ src/main/java/com/kakao/borrowme/user/UserRequest.java
@@ -1,13 +1,15 @@
 package com.kakao.borrowme.user;
 
-import com.kakao.borrowme.university.University;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Pattern;
 
 public class UserRequest {
     @Getter @Setter
     public static class JoinDTO {
         private String universityName;
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$")
         private String email;
         private String password;
         private String nickname;
--- src/main/java/com/kakao/borrowme/user/UserRestController.java
+++ src/main/java/com/kakao/borrowme/user/UserRestController.java
@@ -8,18 +8,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserRestController {
-    private final UserJPARepository userJPARepository;
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO joinDTO) {
-
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO joinDTO) {
         joinDTO.setPassword(passwordEncoder.encode(joinDTO.getPassword()));
-        userJPARepository.save(joinDTO.toEntity());
 
         return ResponseEntity.ok().body("ok");
     }
