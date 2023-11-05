11c2e3271138301d94c1c6f6e9a5a47efe215ce0
geunho00 <gghh1220@gmail.com>
2023-11-05 20:00:42 +0900

    feat: 리뷰등록 기능 구현

--- src/main/java/com/kakao/borrowme/review/ReviewController.java
+++ src/main/java/com/kakao/borrowme/review/ReviewController.java
@@ -1,8 +1,10 @@
 package com.kakao.borrowme.review;
 
+import com.kakao.borrowme._core.security.CustomUserDetails;
 import com.kakao.borrowme._core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +20,16 @@ public class ReviewController {
     public ResponseEntity<?> getReview(@PathVariable Long productId) {
         List<ReviewResponse.ReviewDTO> responseDTOs = reviewService.getReview(productId);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTOs);
+        return ResponseEntity.ok(apiResult);
+    }
+
+    // 리뷰 등록
+    @PostMapping("/review/{productId}")
+    public ResponseEntity<?> postReview(@PathVariable Long productId,
+                                        @RequestBody ReviewRequest.ReviewDTO requestDTO,
+                                        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        reviewService.postReview(productId, requestDTO, userDetails.getUser());
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success((Object)null);
         return ResponseEntity.ok(apiResult);
     }
 }
--- src/main/java/com/kakao/borrowme/review/ReviewService.java
+++ src/main/java/com/kakao/borrowme/review/ReviewService.java
@@ -1,5 +1,10 @@
 package com.kakao.borrowme.review;
 
+import com.kakao.borrowme._core.errors.exception.Exception404;
+import com.kakao.borrowme._core.security.CustomUserDetails;
+import com.kakao.borrowme.product.Product;
+import com.kakao.borrowme.product.ProductJPARepository;
+import com.kakao.borrowme.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +17,7 @@ import java.util.stream.Collectors;
 public class ReviewService {
 
     private final ReviewJPARepository reviewRepository;
+    private final ProductJPARepository productRepository;
 
     @Transactional(readOnly = true)
     public List<ReviewResponse.ReviewDTO> getReview(Long productId) {
@@ -22,5 +28,18 @@ public class ReviewService {
                 .collect(Collectors.toList());
 
         return responseDTOs;
+    }
+
+    @Transactional
+    public void postReview(Long productId, ReviewRequest.ReviewDTO requestDTO, User user) {
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new Exception404("존재하지 않는 제품입니다. : " + productId, "review_not_existed"));
+
+        Review review = new Review();
+        review.updateProduct(product);
+        review.updateStar(requestDTO.getStar());
+        review.updateContent(requestDTO.getContent());
+
+        reviewRepository.save(review);
     }
 }
