31712f4e9fb391aaa851f1dd7459e6bd509ce66f
geunho00 <gghh1220@gmail.com>
2023-11-05 20:52:03 +0900

    refactor: 리뷰등록 기능 예외처리

--- src/main/java/com/kakao/borrowme/review/ReviewService.java
+++ src/main/java/com/kakao/borrowme/review/ReviewService.java
@@ -1,9 +1,7 @@
 package com.kakao.borrowme.review;
 
+import com.kakao.borrowme._core.errors.exception.Exception400;
 import com.kakao.borrowme._core.errors.exception.Exception404;
-import com.kakao.borrowme._core.security.CustomUserDetails;
-import com.kakao.borrowme.product.Product;
-import com.kakao.borrowme.product.ProductJPARepository;
 import com.kakao.borrowme.rental.Rental;
 import com.kakao.borrowme.rental.RentalJPARepository;
 import com.kakao.borrowme.user.User;
@@ -38,6 +36,16 @@ public class ReviewService {
                 () -> new Exception404("존재하지 않는 대여 기록입니다. : " + rentalId, "rental_not_existed")
         );
 
+        int star = requestDTO.getStar();
+        if (star < 0 || star > 5) {
+            throw new Exception400("별점은 0에서 5 사이의 값으로 작성해주세요." + star, "review_star_length");
+        }
+
+        String content = requestDTO.getContent();
+        if (content != null && content.length() > 150) {
+            throw new Exception400("리뷰 내용은 150자 이내로 작성해주세요." + content, "review_content_length");
+        }
+
         Review review = new Review();
         review.updateProduct(rental.getProduct());
         review.updateStar(requestDTO.getStar());
@@ -46,4 +54,3 @@ public class ReviewService {
         reviewRepository.save(review);
     }
 }
-
