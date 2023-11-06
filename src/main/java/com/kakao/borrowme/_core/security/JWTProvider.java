1dae3e3c1b5ad52970ccd20208b2ef581fc76381
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2023-11-06 21:56:26 +0900

    Update JWTProvider.java

--- src/main/java/com/kakao/borrowme/_core/security/JWTProvider.java
+++ src/main/java/com/kakao/borrowme/_core/security/JWTProvider.java
@@ -14,10 +14,21 @@ import java.time.LocalDateTime;
 
 @Component
 public class JWTProvider {
-    public static final Long ACCESS_EXP = 14400L;
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER = "Authorization";
-    public static final String SECRET = "BUklzQUpRMEN6cm5iR0ZTZDB3WjhpNG8tdWF0RmNLeTNrTVhnVGpadFhKT05lMjVMOEx2MHJuUEQyaEhReXNhQW1sdkVBTHdfd2NCcMPL-OPBn4IDkAHv-9SnixKYAQQ";
+    public static Long ACCESS_EXP;
+    public static String TOKEN_PREFIX;
+    public static String HEADER;
+    public static String SECRET;
+
+    public JWTProvider(
+            @Value("${security.jwt.access_exp}") Long ACCESS_EXP,
+            @Value("${security.jwt.token_prefix} ") String TOKEN_PREFIX,
+            @Value("${security.jwt.header}") String HEADER,
+            @Value("${security.jwt.secret}") String SECRET) {
+        this.ACCESS_EXP = ACCESS_EXP;
+        this.TOKEN_PREFIX = TOKEN_PREFIX;
+        this.HEADER = HEADER;
+        this.SECRET = SECRET;
+    }
 
     public static String create(User user) {
         String jwt = JWT.create()
