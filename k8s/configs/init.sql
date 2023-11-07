7f4666b8ced698792d67c4215fe85efe5f1a34ce
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2023-11-08 02:16:31 +0900

    Update init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,6 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `borrowme_db_test` DEFAULT CHARACTER SET utf8mb4;
 
-GRANT ALL ON borrowme_db_test.* TO 'root'@'%';
+CREATE USER 'root'@'%' identified by 'Kakao@123';
+GRANT ALL PRIVILEGES ON borrowme_db_test.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 USE `borrowme_db_test`;
