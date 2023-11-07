e7421792a66546aaa8d9fe8b46360f19e70d719f
Park Chan <117536467+zxc88kr@users.noreply.github.com>
2023-11-07 16:07:46 +0900

    Update init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `borrowme_db` DEFAULT CHARACTER SET utf8mb4;
 
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
+GRANT ALL ON borrowme_db.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
 USE `borrowme_db`;
