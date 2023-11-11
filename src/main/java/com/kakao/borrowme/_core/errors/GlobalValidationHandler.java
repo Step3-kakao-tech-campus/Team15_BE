45177bf4f8ca47594c4927b45cc1d4a5f27cc985
Park Chan <zxc88kr@naver.com>
2023-11-11 20:08:40 +0900

    refactor: 글로벌 유효성 검사 처리 수정

--- src/main/java/com/kakao/borrowme/_core/errors/GlobalValidationHandler.java
+++ src/main/java/com/kakao/borrowme/_core/errors/GlobalValidationHandler.java
@@ -5,6 +5,9 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.Arrays;
 
 public class GlobalValidationHandler {
     @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
@@ -13,15 +16,14 @@ public class GlobalValidationHandler {
     @Before("postMapping()")
     public void validationAdvice(JoinPoint jp) {
         Object[] args = jp.getArgs();
-        for (Object arg : args) {
-            if (arg instanceof Errors) {
-                Errors errors = (Errors) arg;
-                if (errors.hasErrors()) {
-                    throw new Exception400(
-                            errors.getFieldErrors().get(0).getDefaultMessage() + ":" + errors.getFieldErrors().get(0).getField(), ""
-                    );
-                }
-            }
-        }
+        Arrays.stream(args)
+                .filter(arg -> arg instanceof Errors)
+                .forEach(arg -> {
+                    Errors errors = (Errors) arg;
+                    if (errors.hasErrors()) {
+                        FieldError error = errors.getFieldErrors().get(0);
+                        throw new Exception400(error.getDefaultMessage() + ":" + error.getField(), "format");
+                    }
+                });
     }
 }
--- src/main/java/com/kakao/borrowme/user/UserRequest.java
+++ src/main/java/com/kakao/borrowme/user/UserRequest.java
@@ -10,12 +10,12 @@ public class UserRequest {
     @Getter
     @Setter
     public static class JoinDTO {
-        @Pattern(regexp = "^[가-힣]+대학교$")
+        @Pattern(regexp = "^[가-힣]+대학교$", message = "대학교 형식으로 작성해주세요.")
         private String universityName;
-        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요.")
         private String email;
         @Size(min = 8, max = 20)
-        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)\\S*$")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)\\S*$", message = "비밀번호 형식으로 작성해주세요.")
         private String password;
         private String nickname;
     }
@@ -23,17 +23,17 @@ public class UserRequest {
     @Getter
     @Setter
     public static class JoinCheckDTO {
-        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요.")
         private String email;
     }
 
     @Getter
     @Setter
     public static class LoginDTO {
-        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$")
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요.")
         private String email;
         @Size(min = 8, max = 20)
-        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)\\S*$")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)\\S*$", message = "비밀번호 형식으로 작성해주세요.")
         private String password;
     }
 }
